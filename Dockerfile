35b807b3fdb6ecbb3cb7b424b8c64adefd672f07
Sunkist228 <korkishkoegor2019@gmail.com>
2024-05-19 13:48:46 +0300

    modified

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,5 @@
+# Использовать базовый образ Python
 FROM python:3.9-slim
-
-RUN pip install selenium
-
-RUN apt-get update && apt-get install -y \
-	wget \
-	unzip \
-	gnupg \
-	xvfb \
-	iputils-ping && \
-	rm -rf /var/lib/apt/lists/*
 
 # Установка зависимостей Google Chrome
 RUN apt-get update && apt-get install -y \
@@ -16,30 +7,25 @@ RUN apt-get update && apt-get install -y \
 	libasound2 \
 	libatk-bridge2.0-0 \
 	libatk1.0-0 \
-	libatspi2.0-0 \
-	libcairo2 \
 	libcups2 \
-	libcurl3-gnutls \
-	libdbus-1-3 \
+	libdrm2 \
 	libgbm1 \
-	libglib2.0-0 \
-	libgtk-3-0 \
 	libnspr4 \
 	libnss3 \
-	libpango-1.0-0 \
-	libwayland-client0 \
+	libx11-xcb1 \
 	libxcomposite1 \
 	libxdamage1 \
-	libxkbcommon0 \
-	xdg-utils --no-install-recommends && \
+	libxrandr2 \
+	wget \
+	--no-install-recommends && \
 	rm -rf /var/lib/apt/lists/*
 
-# Копирование файлов ChromeDriver и Google Chrome из директории проекта
-COPY src/chromedriver_linux64.zip /tmp/chromedriver.zip
+# Скачивание файлов ChromeDriver и Google Chrome из директорий проекта
+COPY src/chromedriver_linux64.zip /tmp/chromeDriver.zip
 COPY src/google-chrome-stable_current_amd64-107.0.5304.63.deb /tmp/google-chrome.deb
 
 # Установка ChromeDriver
-RUN unzip /tmp/chromedriver.zip -d /usr/local/bin/
+RUN unzip /tmp/chromeDriver.zip -d /usr/local/bin
 
 # Установка Google Chrome
 RUN dpkg -i /tmp/google-chrome.deb && apt-get install -f -y
@@ -48,4 +34,5 @@ RUN dpkg -i /tmp/google-chrome.deb && apt-get install -f -y
 COPY . /app
 WORKDIR /app
 
+# Запуск скрипта
 CMD ["python", "script.py"]
--- script.py
+++ script.py
@@ -3,62 +3,57 @@ import subprocess
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 # Проверка доступа к интернету
 
 
 def check_internet_connection():
     try:
-        # Попытка выполнить ping на google.com
-        subprocess.run(
-            ["ping", "-c", "5", "lmsdo.rea.ru/login/index.php"], check=True)
+        subprocess.check_call(
+            ["ping", "-c", "5", "google.com"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
         return True
     except subprocess.CalledProcessError:
         return False
 
 
-# Если есть доступ к интернету, выполняем скрипт Selenium
+# Основная логика скрипта
 if check_internet_connection():
-    # Настройки Chrome
-    chrome_options = Options()
+    # Настройка Chrome
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
 
     # Запуск ChromeDriver
-    service = Service('/usr/local/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver = webdriver.Chrome(service=Service(
+        "/usr/local/bin/chromedriver"), options=chrome_options)
 
     # Открытие сайта
-    driver.get(
-        '< class="btn btn-primary unti-login-button" href="https://lmsdo.rea.ru/login/index.php')
+    driver.get("https://insdo.rea.ru/login/index.php")
 
-    # Ожидание появления кнопки логина в течение 10 секунд
+    # Ожидание появления кнопки логина
     wait = WebDriverWait(driver, 15)
     login_button = wait.until(
-        EC.presence_of_element_located((By.LINK_TEXT, "Войти с помощью “20.35”")))
-
-    # Клик по кнопке логина
+        EC.presence_of_element_located((By.LINK_TEXT, "Войти с помощью ЭИОС"))
+    )
     login_button.click()
 
     # Ввод логина и пароля
-    driver.find_element(By.ID, 'login').send_keys('egor.k.s@mail.ru')
-    driver.find_element(By.ID, 'password').send_keys('QX64LanY2CkfY8Nx')
-    driver.find_element(By.XPATH, '//button[@type="submit"]').click()
+    driver.find_element(By.ID, "login").send_keys(os.getenv("LOGIN"))
+    driver.find_element(By.ID, "password").send_keys(os.getenv("PASSWORD"))
+    driver.find_element(By.XPATH, "//button[@type='submit']").click()
 
-    time.sleep(15)  # Подождите, пока выполнится вход
-
-    # Найдите все видео и просмотрите их полностью
-    videos = driver.find_elements(By.TAG_NAME, 'video')
+    # Поиск и воспроизведение видео
+    wait.until(EC.presence_of_all_elements_located((By.TAG_NAME, "video")))
+    videos = driver.find_elements(By.TAG_NAME, "video")
     for video in videos:
-        duration = driver.execute_script(
-            "return arguments[0].duration;", video)
-        driver.execute_script("arguments[0].play();", video)
-        time.sleep(duration)  # Ждать продолжительность видео
+        driver.execute_script("return arguments[0].play();", video)
+        time.sleep(15)  # Ждать 15 секунд воспроизведения видео
 
     driver.quit()
 else:
